b27d4d7d1e69bc089ad8f058dfc477c6a2096607
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-18 11:13:03 -0300

    aula 2 - instalado cors para lidar com quais front ends poderão acessar a API

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import fastify from "fastify";
+import cors from '@fastify/cors';
 import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
+
+app.register(cors, {
+    origin: true, //todo mundo pode acessar a API
+    // origin: ['localhost:3000'] //posso colocar uma lista com todas as URLs q podem acessar
+});
 
 //registrando as rotas
 app.register(memoriesRoutes);
